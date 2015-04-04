b9c514df47c203ca21dde5231c88b5168e604ca1
Sebastian Maier <sebastianmaier@users.noreply.github.com>
2015-04-04 22:18:38 +0200

    Update mongoid.rb
    the attribute_will_change! method seems to break the _was method in the dirty module

--- lib/carrierwave/mongoid.rb
+++ lib/carrierwave/mongoid.rb
@@ -65,14 +65,6 @@ module CarrierWave
           changed_attributes.has_key?("#{column}")
         end
 
-        # The default Mongoid attribute_will_change! method is not enough
-        # when we want to upload a new file in an existing embedded document.
-        # The custom version of that method forces the callbacks to be
-        # ran and so does the upload.
-        def #{column}_will_change!
-          changed_attributes["#{column}"] = '_new_'
-        end
-
         def find_previous_model_for_#{column}
           if self.embedded?
             if self.respond_to?(:__metadata) # Mongoid >= 4.0.0.beta1
